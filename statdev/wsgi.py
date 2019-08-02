ede19d699ff967f8a795ac4b123f66ef2feebdce
jason.moore@dbca.wa.gov.au <Jason Moore>
2019-08-02 15:55:35 +0800

    BASE_DIR wsgi fix

--- statdev/wsgi.py
+++ statdev/wsgi.py
@@ -5,8 +5,9 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 import confy
 from django.core.wsgi import get_wsgi_application
 import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-confy.read_environment_file(".env")
+confy.read_environment_file(BASE_DIR+"/.env")
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "statdev.settings")
 application = get_wsgi_application()
