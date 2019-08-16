a6a44707e96429d18595786e6f9b4a0f4e92604b
jason.moore@dbca.wa.gov.au <Jason Moore>
2019-08-16 14:00:55 +0800

    Fix Group

--- approvals/views.py
+++ approvals/views.py
@@ -118,7 +118,7 @@ class ApprovalList(ListView):
 #       context['app_list'] = context['app_list'].order_by('title')
 
         # TODO: any restrictions on who can create new applications?
-        processor = Group.objects.get(name='Processor')
+        processor = Group.objects.get(name='Statdev Processor')
 
         # Rule: admin officers may self-assign applications.
         if processor in self.request.user.groups.all() or self.request.user.is_superuser:
